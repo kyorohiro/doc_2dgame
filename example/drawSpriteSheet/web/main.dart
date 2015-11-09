218ee992c2d9003ad022315a89e29e56798a37db
kyorohiro <kyorohiro@gmail.com>
2015-11-09 21:04:54 +0900

    ..

--- example/drawSpriteSheet/web/main.dart
+++ example/drawSpriteSheet/web/main.dart
@@ -1,8 +1,8 @@
 import 'package:umiuni2d/tinygame.dart';
 import 'package:umiuni2d/tinygame_webgl.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 main() async {
-  print("---------NNNN---1--");
   TinyGameBuilder builder = new TinyGameBuilderForWebgl();
   TinyGameRoot root = new TinyGameRoot(400.0, 300.0);
   TinyStage stage = builder.createStage(root);
@@ -15,8 +15,7 @@ class Test extends TinyDisplayObject {
   TinyImage image = null;
 
   Test(TinyGameBuilder builder) {
-    print("---------NNNN----2-");
-    builder.loadImage("./assets/nono.png").then((TinyImage i){
+    builder.loadImage("./assets/nono.png").then((TinyImage i) {
       image = i;
     });
     builder.loadString("./assets/nono.json").then((String x) {
@@ -33,28 +32,24 @@ class Test extends TinyDisplayObject {
   int i = 0;
   int d = 0;
   void onPaint(TinyStage stage, TinyCanvas canvas) {
-    if(spriteInfo == null) {
+    if (spriteInfo == null) {
       return;
     }
     d++;
-    if(d<4) {
-      
-    } else {
-    d=0;
-    i++;
+    if (d < 2) {} else {
+      d = 0;
+      i++;
     }
-    int index = i%spriteInfo.frames.length;
+    int index = i % spriteInfo.frames.length;
 
+    canvas.pushMulMatrix(new Matrix4.identity()..translate(50.0, 0.0, 0.0));
     TinyPaint paint =
         new TinyPaint(color: new TinyColor.argb(0xaa, 0xff, 0xaa, 0xaa));
-//    canvas.drawRect(stage, new TinyRect(100.0, 100.0, 150.0, 50.0), paint);
-    if(image != null) {
-    canvas.drawImageRect(stage, image, 
-        spriteInfo.frames[index].srcRect, 
-        spriteInfo.frames[index].dstRect,
-        paint);
+    if (image != null) {
+      canvas.drawImageRect(stage, image, spriteInfo.frames[index].srcRect,
+          spriteInfo.frames[index].dstRect, paint);
+      canvas.popMatrix();
     }
-    //canvas.drawRect(stage, new TinyRect(100.0, 100.0, 150.0, 50.0), paint);
   }
 
   bool onTouch(TinyStage stage, int id, String type, double x, double y,
