8afa64e9bc4956ac13ecbf16e5ea01381980b353
kyorohiro <kyorohiro@gmail.com>
2015-11-10 17:35:24 +0900

    ..

---
+++ example/touchTest/lib/touch_test.dart
@@ -0,0 +1,28 @@
+import 'package:umiuni2d/tinygame.dart';
+
+class PrimitiveTest extends TinyDisplayObject {
+  TinyGameBuilder builder;
+  PrimitiveTest(this.builder) {}
+
+  Map<int, List<double>> touches = {};
+
+  bool onTouch(TinyStage stage, int id, String type, double x, double y,
+      double globalX, globalY) {
+    touches[id] = [x, y];
+    if (type == "pointerup" || type == "pointercancel") {
+      touches.remove(id);
+    }
+    return false;
+  }
+
+  void onPaint(TinyStage stage, TinyCanvas canvas) {
+    TinyPaint p = new TinyPaint();
+    for (List<double> xy in touches.values) {
+      p.color = new TinyColor.argb(0xff, 0x00, 0xff, 0xff);
+      p.style = TinyPaintStyle.stroke;
+      p.strokeWidth = 2.5;
+      canvas.drawRect(
+          null, new TinyRect(xy[0] - 25.0, xy[1] - 25.0, 50.0, 50.0), p);
+    }
+  }
+}
