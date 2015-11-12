f9b317dd8a3ee94c2ed3d104a12575b09812a94f
kyorohiro <kyorohiro@gmail.com>
2015-11-12 18:19:46 +0900

    ..

--- demo/mino/lib/game.dart
+++ demo/mino/lib/game.dart
@@ -6,17 +6,84 @@ import 'package:umiuni2d/tinygame.dart';
 
 class MinoRoot extends TinyDisplayObject {
   TinyGameBuilder builder;
+  MinoGame game = new MinoGame();
   MinoRoot(this.builder) {
-    addChild(new MinoTableUI(builder));
+    addChild(new MinoTableUI(builder, game.table));
+  }
+
+  int time = 0;
+  void onTick(TinyStage stage, int timeStamp) {
+    if(time > 2) {
+      game.loop();      
+      time=0;
+    }
+    time++;
+  }
+}
+
+class MinoGame {
+  MinoTable table = new MinoTable();
+  Minon minon = new Minon.l();
+
+  MinoGame() {
+    nextMinon();
+  }
+
+  loop() {
+    setMinon(minon, true);
+    down();
+  }
+
+  nextMinon() {
+    minon = new Minon.l();
+    minon.x = table.fieldWWithFrame ~/ 2;
+  }
+  down() {
+    setMinon(minon, false);
+    minon.y++;
+    if(collision(minon)) {
+      minon.y--;
+      setMinon(minon, true);
+      nextMinon();
+      setMinon(minon, true);
+    } else {
+      setMinon(minon, true);      
+    }
+  }
+  
+  bool collision(Minon minon) {
+    for (MinonElm e in minon.minos) {
+      Mino m = table.getMino(minon.x + e.x, minon.y + e.y);
+      if(!(m.type == MinoTyoe.empty || m.type == MinoTyoe.out)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  setMinon(Minon minon, bool on) {
+    for (MinonElm e in minon.minos) {
+      Mino m = table.getMino(minon.x + e.x, minon.y + e.y);
+      if (m.type != MinoTyoe.out) {
+        if (on == true) {
+          m.type = e.type;
+        } else {
+          m.type = MinoTyoe.empty;
+        }
+      }
+    }
   }
 }
 
 class MinoTableUI extends TinyDisplayObject {
   TinyGameBuilder builder;
-  MinoTable table = new MinoTable();
+  MinoTable table;
   TinyColor colorEmpty = new TinyColor.argb(0xaa, 0xff, 0xaa, 0xaa);
   TinyColor colorFrame = new TinyColor.argb(0xaa, 0x55, 0x33, 0x33);
-  MinoTableUI(this.builder) {
+  TinyColor colorL = new TinyColor.argb(0xaa, 0xaa, 0xff, 0xaa);
+
+  MinoTableUI(this.builder, this.table) {
     ;
   }
 
@@ -34,6 +101,8 @@ class MinoTableUI extends TinyDisplayObject {
           p.color = colorFrame;
         } else if (table.getMino(x, y).type == MinoTyoe.empty) {
           p.color = colorEmpty;
+        } else if (table.getMino(x, y).type == MinoTyoe.l) {
+          p.color = colorL;
         }
         canvas.drawRect(stage, rect, p);
       }
@@ -49,10 +118,11 @@ class MinoTable {
   int get fieldWWithFrame => fieldW + 2;
   int get fieldHWithFrame => fieldH + 1;
 
-  MinoTable({this.fieldW: 12, this.fieldH: 22}) {
+  Mino outMino = new Mino(MinoTyoe.out);
+  MinoTable({this.fieldW: 11, this.fieldH: 21}) {
     for (int y = 0; y < fieldHWithFrame; y++) {
       for (int x = 0; x < fieldWWithFrame; x++) {
-        if (x == 0 || x == fieldWWithFrame-1 || y == fieldH) {
+        if (x == 0 || x == fieldWWithFrame - 1 || y == fieldH) {
           minos.add(new Mino(MinoTyoe.frame));
         } else {
           minos.add(new Mino(MinoTyoe.empty));
@@ -62,11 +132,34 @@ class MinoTable {
   }
 
   Mino getMino(int x, int y) {
+    if (x < 0 || x >= fieldHWithFrame || y < 0 || y >= fieldHWithFrame) {
+      return outMino;
+    }
     return minos[x + y * fieldWWithFrame];
   }
 }
 
-enum MinoTyoe { empty, frame, l, o, s, z, j, t }
+enum MinoTyoe { empty, frame, out, l, o, s, z, j, t }
+
+class Minon {
+  int x = 0;
+  int y = 0;
+  List<MinonElm> minos = [];
+  Minon.l() {
+    minos.add(new MinonElm(MinoTyoe.l, 0, 0));
+    minos.add(new MinonElm(MinoTyoe.l, -1, 0));
+    minos.add(new MinonElm(MinoTyoe.l, 1, 0));
+    minos.add(new MinonElm(MinoTyoe.l, 2, 0));
+  }
+}
+
+class MinonElm extends Mino {
+  int x;
+  int y;
+  MinonElm(MinoTyoe type, this.x, this.y) : super(type) {
+    ;
+  }
+}
 
 class Mino {
   MinoTyoe type;
