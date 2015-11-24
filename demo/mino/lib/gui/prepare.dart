2184923a2462f3d34d30a510272c3651f7d98e57
kyorohiro <kyorohiro@gmail.com>
2015-11-25 08:19:07 +0900

    mod setting image

--- demo/mino/lib/gui/prepare.dart
+++ demo/mino/lib/gui/prepare.dart
@@ -33,24 +33,25 @@ class PrepareScene extends TinyDisplayObject {
 
     TinyButton level1 = new TinyButton("L01", 45.0, 45.0, onLevelButton);
     level1.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
-    level1.mat.translate(50+70.0,50.0,0.0);
-
+    level1.mat.translate(50+20.0,50.0,0.0);
+    
     TinyButton level2 = new TinyButton("L02", 45.0, 45.0, onLevelButton);
     level2.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
-    level2.mat.translate(50+125.0,50.0,0.0);
+    level2.mat.translate(50+70.0,50.0,0.0);
 
     TinyButton level3 = new TinyButton("L03", 45.0, 45.0, onLevelButton);
     level3.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
-    level3.mat.translate(50+165.0,50.0,0.0);
+    level3.mat.translate(50+125.0,50.0,0.0);
 
     TinyButton level4 = new TinyButton("L04", 45.0, 45.0, onLevelButton);
     level4.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
-    level4.mat.translate(50+215.0,50.0,0.0);
+    level4.mat.translate(50+165.0,50.0,0.0);
 
     TinyButton level5 = new TinyButton("L05", 45.0, 45.0, onLevelButton);
     level5.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
-    level5.mat.translate(50+265.0,50.0,0.0);
-    
+    level5.mat.translate(50+215.0,50.0,0.0);
+
+
     addChild(level1);
     addChild(level2);
     addChild(level3);
@@ -58,19 +59,20 @@ class PrepareScene extends TinyDisplayObject {
     addChild(level5);
 
     no1 = new ScoreUI(null, null);
-    no1.mat.translate(120.0,140.0,0.0);
+    no1.mat.translate(90.0,220.0,0.0);
     no2 = new ScoreUI(null, null);
-    no2.mat.translate(150.0,180.0,0.0);
+    no2.mat.translate(90.0,247.0,0.0);
     no3 = new ScoreUI(null, null);
-    no3.mat.translate(180.0,220.0,0.0);
+    no3.mat.translate(90.0,278.0,0.0);
     addChild(no1);
     addChild(no2);
     addChild(no3);
 
     level = 1;
     onLevelButton("L01");
-    TinyButton startB = new TinyButton("start", 150.0, 50.0, onStartButton);
-    startB.mat.translate(240.0,250.0,0.0);
+    TinyButton startB = new TinyButton("start", 170.0, 50.0, onStartButton);
+    startB.mat.translate(230.0,250.0,0.0);
+    startB.bgcolorOff = new TinyColor.argb(0x00, 0xff, 0xff, 0xff);
     addChild(startB);
   }
   
@@ -80,27 +82,27 @@ class PrepareScene extends TinyDisplayObject {
     print("touch # ${id}");
     switch(id) {
       case "L01":
-        chX = 120.0;
+        chX = 70.0;
         chY = 60.0;
         level = 0;
         break;
       case "L02":
-        chX = 175.0;
+        chX = 125.0;
         chY = 60.0;
         level = 1;
         break;
       case "L03":
-        chX = 215.0;
+        chX = 175.0;
         chY = 60.0;
         level = 2;
         break;
       case "L04":
-        chX = 265.0;
+        chX = 215.0;
         chY = 60.0;
         level = 3;
         break;
       case "L05":
-        chX = 315.0;
+        chX = 265.0;
         chY = 60.0;
         level = 4;
         break;
