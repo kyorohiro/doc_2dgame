e9532316e1177d256d405787609309b0ebb37817
kyorohiro <kyorohiro@gmail.com>
2015-12-14 19:06:45 +0900

    ..

---
+++ example/drawImageTransform/lib/main.dart
@@ -0,0 +1,28 @@
+//
+//
+// Flutter entry point
+//
+//
+import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart';
+import 'package:umiuni2d/tinygame.dart';
+import 'package:umiuni2d/tinygame_flutter.dart';
+import 'package:trans_image/trans_image_test.dart';
+import 'dart:async';
+
+void main() {
+  runApp(new GameWidget());
+}
+
+class GameWidget extends OneChildRenderObjectWidget {
+  GameWidget() {}
+  RenderObject createRenderObject() {
+    TinyGameBuilderForFlutter builder = new TinyGameBuilderForFlutter("web/");
+    TinyGameRoot root = new TinyGameRoot(400.0, 300.0);
+    TinyStage stage = builder.createStage(root);
+    (stage as TinyFlutterStage).isNCanvas = true;
+    stage.start();
+    stage.root.addChild(new PrimitiveTest(builder));
+    return (stage as TinyFlutterStage);
+  }
+}
---
+++ example/drawImageTransform/lib/trans_image_test.dart
@@ -0,0 +1,65 @@
+import 'dart:math' as math;
+import 'package:umiuni2d/tinygame.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+class PrimitiveTest extends TinyDisplayObject {
+  TinyGameBuilder builder;
+  TinyImage image = null;
+  PrimitiveTest(this.builder) {
+    builder.loadImage("assets/icon.png").then((TinyImage i) {
+      image = i;
+    });
+  }
+
+  void onPaint(TinyStage stage, TinyCanvas canvas) {
+    if(image == null) {
+      return;
+    }
+    {
+      TinyPaint p = new TinyPaint();
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(0.0,50.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.NONE);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(0.0,100.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.ROT90);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(0.0,150.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.ROT180);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(0.0,200.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.ROT270);
+    }
+    {
+      TinyPaint p = new TinyPaint();
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(100.0,50.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.MIRROR);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(100.0,100.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.MIRROR_ROT90);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(100.0,150.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.MIRROR_ROT180);
+      canvas.drawImageRect(stage,
+          image, 
+          new TinyRect(0.0,0.0, 1.0*image.w, 1.0*image.h), 
+          new TinyRect(100.0,200.0, 40.0, 40.0),
+          p,transform: TinyCanvasTransform.MIRROR_ROT270);
+    }
+  }
+}
